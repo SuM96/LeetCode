c52efd538607489e149f1d317cf7e49a591df959
Martin <28719783+SuM96@users.noreply.github.com>
2019-12-13 12:56:34 -0800

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,9 +1,14 @@
+/*
+Problem Website: https://leetcode.com/problems/two-sum/
+*/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> solution(2);
         for(int i = 0; i < nums.size(); i++) {
             for(int j = i + 1; j < nums.size(); j++) {
+                // If the sums matches, return the indices on where they match.
                 if(nums[i] + nums[j] == target) {
                     solution[0] = i;
                     solution[1] = j;
