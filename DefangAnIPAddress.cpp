90919f8f2ad49e2799c2ea21f6d00b09192f9dad
Martin <28719783+SuM96@users.noreply.github.com>
2019-12-13 13:08:23 -0800

    Update DefangAnIPAddress.cpp

--- DefangAnIPAddress.cpp
+++ DefangAnIPAddress.cpp
@@ -4,6 +4,7 @@ Problem Website: https://leetcode.com/problems/defanging-an-ip-address
 class Solution {
 public:
     string defangIPaddr(string address) {
+        // Decided to make my own string in this approach. It might be faster if I utilize substr and edit the substring as a go.
         string defangedIP = "";
         for(int i = 0; i < address.length(); i++) {
             if(address[i] == '.') {
